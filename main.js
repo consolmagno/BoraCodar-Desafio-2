b30a5803dc278609a2c301d914237c2207894b99
Sofia Consolmagno Fontes <sofiaconsolmagno@hotmail.com>
2023-01-16 16:00:40 -0300

    function to change image to gif when click

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const iconButtonTurn = document.querySelector('.turn')
 const iconButtonClose = document.querySelector('.close')
+const sofaImage = document.querySelector('.img')
+const sofaGif = document.querySelector('.gif')
 
-iconButtonTurn.addEventListener('click', function () {
+function toggleBtn() {
   iconButtonClose.classList.toggle('hide')
   iconButtonTurn.classList.toggle('hide')
-})
+  sofaImage.classList.toggle('hide')
+  sofaGif.classList.toggle('hide')
+}
 
-iconButtonClose.addEventListener('click', function () {
-  iconButtonClose.classList.toggle('hide')
-  iconButtonTurn.classList.toggle('hide')
-})
+iconButtonTurn.addEventListener('click', toggleBtn)
+iconButtonClose.addEventListener('click', toggleBtn)
